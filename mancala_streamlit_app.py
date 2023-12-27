a98788c20db53ab6fbd6594f78b07bd8aeb4a143
Jonathan Hanke <jonhanke@gmail.com>
2023-12-27 00:15:18 -0500

    Moved the mancala streamlit app upwards to allow it to import the mancala module.

--- mancala_streamlit_app.py
+++ mancala_streamlit_app.py
@@ -2,12 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-from ...mancala import *
+from .mancala import *
 
+## Define the game board instance
 game = Mancala()
+
 
 ## Create the app title
 st.title('Mancala')
+
 
 ## Show the game board
 game
