f0025ce001e8fd00891c509f04f7ecfdc70bc1f5
Jonathan Hanke <jonhanke@gmail.com>
2023-12-27 00:08:10 -0500

    Added bare webpage for streamlit Mancala app.

---
+++ apps/streamlit_app/mancala_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+from ...mancala import *
+
+game = Mancala()
+
+## Create the app title
+st.title('Mancala')
+
+## Show the game board
+game
+
+## Get the next move
+move = st.text_input("Enter your move ('Q' to quit): ")
