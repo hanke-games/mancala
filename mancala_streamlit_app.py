6526968efa4d012b44860837f9feb1280e252608
Jonathan Hanke <jonhanke@gmail.com>
2023-12-27 00:54:44 -0500

    Added code to show the game history.

--- mancala_streamlit_app.py
+++ mancala_streamlit_app.py
@@ -8,12 +8,24 @@ from mancala import *
 game = Kalah(m=6, n=4)
 
 
+
 ## Create the app title
 st.title('Mancala')
-
 
 ## Show the game board
 st.write(game)
 
+
+
 ## Get the next move
 move = st.text_input("Enter your move ('Q' to quit): ")
+
+## Apply the move
+game.perform_move(int(move))
+
+
+## Show the game history
+st.write("Game History:")
+st.write(game.history_list())
+
+
